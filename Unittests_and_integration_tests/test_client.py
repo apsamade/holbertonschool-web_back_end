6007701b9eeba56f059cbdeb50c4723b31f0cf9e
bouderga abdel-samade <bouderga.abdessamade@gmail.com>
2026-09-18 03:33:47 +0200

    register user

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -63,8 +63,8 @@ class TestGithubOrgClient(unittest.TestCase):
         ({"license": {"key": "my_license"}}, "my_license", True),
         ({"license": {"key": "other_license"}}, "my_license", False),
     ])
-    def test_has_license(
-            self, repo: Dict, license_key: str, expected: bool) -> None:
+    def test_has_license(self, repo: Dict,
+                         license_key: str, expected: bool) -> None:
         """Test that has_license correctly compares license keys."""
         self.assertEqual(
             GithubOrgClient.has_license(repo, license_key), expected)
